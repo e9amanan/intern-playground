0d73fbe47298bc041b60a8cb7d48619b505942ac
Aditya Manan <amanan@enine.co.in>
2026-07-08 16:05:35 +0530

    day4

--- week1day4/energy_insights/core.py
+++ week1day4/energy_insights/core.py
@@ -11,4 +11,6 @@ def compute_daily_averages(rows,ts_col,value_col):
 
     return {date:totals[date]/counts[date]for date in total}
 
-def find_spikes
+def find_spikes(rows, value_col,top):
+    sorted_rows = sorted(rows,key=lambda x: float(x[value_col]),reverse = True)
+    return sorted_rows[:top]
