626aa7959c1bfe6378bece77e973a5231c569781
aditiyamanan-bit <aditiyamanan@gmail.com>
2026-07-15 15:58:18 +0530

    w2d4

---
+++ week2day4/cli_project/yourpkg/exceptions.py
@@ -0,0 +1,9 @@
+"""Custom exceptions for the package."""
+
+class FileProcessingError(Exception):
+    """raised when a file does not exist or cannot be read"""
+    pass
+
+class ValidationError(Exception):
+    """custom exception for data validation and formatting errors"""
+    pass
