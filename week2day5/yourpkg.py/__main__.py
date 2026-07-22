83e7e569408cfec63360766f20d3e5a99be7de68
Aditya Manan <amanan@enine.co.in>
2026-07-22 12:16:24 +0530

    fix:
    - remove the venv from repo
    - fix import errors

--- week2day5/yourpkg.py/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Package execution entrypoint."""
-
-from yourpkg.cli import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
---
+++ week2day5/yourpkg/__main__.py
@@ -0,0 +1,6 @@
+"""Package execution entrypoint."""
+
+from week2day5.yourpkg.cli import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
