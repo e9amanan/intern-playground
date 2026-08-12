fd98733a2b69015cbfe068422671161f474e4316
Aditya Manan <amanan@enine.co.in>
2026-08-12 19:28:29 +0530

    week5weekendhalf

--- week5weekend/tasks/models.py
+++ week5weekend/tasks/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    description=models.TextField(blank=True,null=True)
+
+    class Meta:
+        verbose_name_plural="Categories"
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
+
+class TaskManager(models.Manager):
+    def completed_tasks(self):
+        return self.filter(status='DONE')
+
+    def pending_tasks(self):
+        return self.exclude(status='DONE')
+    
+class Task(models.Model):
+    STATUS_CHOICES=[
+        ('TODO','To Do'),
+        ('IN_PROGRESS', 'In_Progress'),
+        ('DONE','Done'),
+    ]
+
+    PRIORITY_CHOICES=[
+        ('LOW','low'),
+        ('MEDIUM','Medium'),
+        ('HIGH','High'),
+    ]
+
+    title=models.CharField(max_length=200)
+    description=models.TextField(blank=True,null=True)
+    status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='TODO')
+    priority=models.CharField(max_length=20,choices=PRIORITY_CHOICES,default='MEDIUM')
+    due_date=models.DateField(blank=True,null=True)
+
+    category=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True,related_name='tasks')
+    tags=models.ManyToManyField(Tag,blank=True)
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_st=models.DateTimeField(auto_now=True)
+
+    objects=TaskManager()
+
+    class Meta:
+        ordering=['-priority','due_data']
+
+    def __str__(self):
+        return self.title
+
+    
--- week5weekend/tasks/views.py
+++ week5weekend/tasks/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from .models import Task
+from .models import TaskForm
+from django.db.models import Q
 
-# Create your views here.
+class TaskListView(ListView):
+    model=Task
+    template_name='tasks/task_list.html'
+    context_object_name='tasks'
+    paginate_by =10
+    
+    def get_queryset(self):
+        queryset=super().get_queryset()
+
+        search_query=self.request.GET.get('search')
+        if search_query:
+            queryset=queryset.filter(title__icontains=search_query)
+
+        status=self.request.GET.get('status')
+        priority=self.request.GET.get('priority')
+
+        if status:
+            queryset=queryset.filter(status=status)
+
+
