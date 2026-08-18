3ec5314589aa124d64d82f235eab0f7c624c8790
aditiyamanan-bit <aditiyamanan@gmail.com>
2026-08-18 12:48:17 +0530

    week6day2

---
+++ week6day2/tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('', views.task_list, name='task_list'),
+    path('batch-create/', views.batch_task_create, name='batch_task_create'),
+    path('ajax-create/', views.ajax_task_create, name='ajax_task_create'),
+]
